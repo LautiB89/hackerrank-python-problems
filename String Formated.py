2aa45340e59d21c4c5bed5e835a9307acb15417c
LautiB89 <83650262+LautiB89@users.noreply.github.com>
2021-07-22 22:15:31 -0300

    Add files via upload

---
+++ String Formated.py
@@ -0,0 +1,12 @@
+def print_formatted(number):
+    # your code goes here
+    width = len(str(bin(number)[2:]))
+    number_list = [" ".join([str(i).rjust(width),
+                             oct(i)[2:].rjust(width),
+                             hex(i).upper()[2:].rjust(width),
+                             bin(i)[2:].rjust(width)]) for i in range(1,number+1)]
+    print("\n".join(number_list))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
